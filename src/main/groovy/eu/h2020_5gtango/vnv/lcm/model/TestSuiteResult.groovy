9b8062604c5a1e27c16ab01a17545b42d0f24a94
Guo Du <guo.du@huawei.com>
2018-03-07 16:53:37 +0000

    expose test execution rest api for lcm #256 * refactory to meet TEE requirements

--- src/main/groovy/eu/h2020_5gtango/vnv/lcm/model/TestSuiteResult.groovy
+++ src/main/groovy/eu/h2020_5gtango/vnv/lcm/model/TestSuiteResult.groovy
@@ -4,17 +4,31 @@ import groovy.transform.EqualsAndHashCode
 import io.swagger.annotations.ApiModelProperty
 
 import javax.validation.constraints.NotNull
+import java.security.MessageDigest
 
 @EqualsAndHashCode
 class TestSuiteResult {
 
+    @ApiModelProperty(required = true)
+    @NotNull
+    String testSuiteResultId
+
+    @ApiModelProperty(required = true)
+    @NotNull
     String testPlanId
+
+    @ApiModelProperty(required = true)
+    @NotNull
     String networkServiceId
+
+    @ApiModelProperty(required = true)
+    @NotNull
     String testSuiteId
 
     String status
 
-    String generateId(){
-        "$testPlanId:$networkServiceId:$testSuiteId"
+    TestSuiteResult buildId(){
+        testSuiteResultId=MessageDigest.getInstance("MD5").digest("$testPlanId:$networkServiceId:$testSuiteId".bytes).encodeHex().toString()
+        this
     }
 }
