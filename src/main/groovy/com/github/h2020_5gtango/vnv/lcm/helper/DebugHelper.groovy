2f1d9b1f1a8143605dc0c8ee92a8e2587fd0595a
Giorgos Andreou <allemaos@gmail.com>
2018-08-22 09:43:54 +0100

    fixing logging message

--- src/main/groovy/com/github/h2020_5gtango/vnv/lcm/helper/DebugHelper.groovy
+++ src/main/groovy/com/github/h2020_5gtango/vnv/lcm/helper/DebugHelper.groovy
@@ -6,8 +6,9 @@ import org.springframework.http.ResponseEntity
 @Log
 class DebugHelper {
     static def callExternalEndpoint(ResponseEntity responseEntity, def methodName, def endpoint, String message="ERROR CONNECTING WITH ENDPOINT"){
-        log.info("##vnvlog:$methodName call_endpoint: $endpoint, status: ${responseEntity.statusCode}")
-        if(responseEntity.statusCodeValue != 200) {
+        if(responseEntity.statusCodeValue in [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]) {
+            log.info("##vnvlog:$methodName call_endpoint: $endpoint, status: ${responseEntity.statusCode}")
+        } else {
             log.error("##vnvlog:$methodName $message: $endpoint, status: ${responseEntity.statusCode}")
             return null
         }
