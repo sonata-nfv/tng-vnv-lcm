d14ce0f74d515ae6ec0fe8224f80fb7e69e1c01f
Giorgos Andreou <allemaos@gmail.com>
2018-08-28 08:22:16 +0100

    resolving null's in test_plans,test_suite_results, network_service_instances

--- src/main/groovy/com/github/h2020_5gtango/vnv/lcm/workflow/WorkflowManager.groovy
+++ src/main/groovy/com/github/h2020_5gtango/vnv/lcm/workflow/WorkflowManager.groovy
@@ -72,15 +72,19 @@ class WorkflowManager {
         log.info("##vnvlog: (networkServiceId: $networkService.networkServiceId, testListSize: ${testSuites.size()})")
         log.info("##vnvlog: issue!:testSuites.first().packageId: ${testSuites.first().packageId}")
         def testPlanUuid = UUID.randomUUID().toString()
+        def nsi = [new NetworkServiceInstance(instanceUuid: UUID.randomUUID().toString(), serviceUuid: networkService.networkServiceId)]
         def testPlan = new TestPlan(
                 uuid: testPlanUuid,
                 packageId: testSuites.first().packageId,
-                networkServiceInstances: [new NetworkServiceInstance(serviceUuid: networkService.networkServiceId)],
+                networkServiceInstances: nsi,
                 testSuiteResults: testSuites.collect {testSuite->
                     new TestSuiteResult(
+                            uuid: UUID.randomUUID().toString(),
                             testUuid: testSuite.testUuid,
                             packageId: testSuite.packageId,
-                            testPlanId: testPlanUuid,
+                            serviceUuid: networkService.networkServiceId,
+                            instanceUuid: nsi.instanceUuid,
+                            testPlanId: testPlanUuid
                     )
                 },
                 status: 'CREATED',
