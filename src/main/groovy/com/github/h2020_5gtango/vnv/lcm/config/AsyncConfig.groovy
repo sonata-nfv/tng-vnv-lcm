4ab9a92d5572386eb1fa40c8e010f84489e29ada
Giorgos Andreou <allemaos@gmail.com>
2018-09-17 17:36:19 +0100

    move concurrency parameters in properties.yml file

--- src/main/groovy/com/github/h2020_5gtango/vnv/lcm/config/AsyncConfig.groovy
+++ src/main/groovy/com/github/h2020_5gtango/vnv/lcm/config/AsyncConfig.groovy
@@ -1,7 +1,9 @@
 package com.github.h2020_5gtango.vnv.lcm.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
@@ -11,12 +13,21 @@ import java.util.concurrent.Executor
 @Configuration
 class AsyncConfig {
 
+    @Value('${app.pool.size.core}')
+    int corePoolSize
+
+    @Value('${app.pool.size.max}')
+    int maxPoolSize
+
+    @Value('${app.queue.capacity}')
+    int queueCapacity
+
     @Bean
     public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
         executor.setThreadNamePrefix("lcm-scheduler-");
         executor.initialize();
         return executor;
